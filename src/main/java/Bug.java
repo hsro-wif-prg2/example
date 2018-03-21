3321c19cff88af040d35e5432eb3e913017c6bb5
feridtola <ferid-tola@hotmail.de>
2018-03-21 20:55:25 +0100

    LoesungExample

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -6,9 +6,15 @@ public class Bug {
 	 * @throws IllegalArgumentException if referenced array is null or empty
 	 */
 	public static int minimum(int[] a) throws IllegalArgumentException {
+
+
+		// abfragen ob das Array null ist oder die Länge 0 hat. Wenn einer zutrifft dann Expection werfen!
+		if(a == null || a.length == 0)
+			throw new IllegalArgumentException();
+		// "m" darf nur dann initialisiert werden wenn das Array weder null noch die Länge null hat!
 		int m = a[0];
 		for (int i = 1; i < a.length; i++) {
-			if (a[i] > m) {
+			if (a[i] < m) {
 				m = a[i];
 			}
 		}
