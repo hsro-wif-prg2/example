7fc27b80c2142f9f1604a2d800f2c0fc95820b64
Gornung <37617440+Gornung@users.noreply.github.com>
2018-04-06 00:31:13 +0200

    Bug gefixxt

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -1,14 +1,21 @@
+import java.util.logging.SocketHandler;
+
 public class Bug {
 	/**
 	 * Find the minimum in the referenced array.
+	 *
 	 * @param a Array of int values
 	 * @return smallest value on the array
 	 * @throws IllegalArgumentException if referenced array is null or empty
 	 */
 	public static int minimum(int[] a) throws IllegalArgumentException {
+		if (a == null || a.length == 0) {
+			throw new IllegalArgumentException();
+		}
+
 		int m = a[0];
-		for (int i = 1; i < a.length; i++) {
-			if (a[i] > m) {
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] < m) {
 				m = a[i];
 			}
 		}
